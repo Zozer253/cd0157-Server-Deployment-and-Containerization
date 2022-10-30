f2a323decf3c8c31d7965949c8b8d5200c8a534b
zozer253 <18ml253@esisalama.org>
2022-10-31 00:46:59 +0200

    Votre branche est à jour avec 'origin/master'. 	modifié :         buildspec.yml 	modifié :         test_main.py

--- test_main.py
+++ test_main.py
@@ -22,10 +22,13 @@ def client():
 
 
 
+
 def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
+
+    assert False
 
 
 def test_auth(client):
